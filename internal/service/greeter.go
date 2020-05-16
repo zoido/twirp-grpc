aa2c4b7193ade73468a60ac9b329a0c15c8e7e1b
Luboš Pokorný <lubos.pokorny@gmail.com>
2020-05-16 15:47:11 +0200

    Cleanup

--- internal/service/greeter.go
+++ internal/service/greeter.go
@@ -5,12 +5,11 @@ import (
 
 	"golang.org/x/net/context"
 
-	"github.com/rs/zerolog/log"
 	"github.com/twitchtv/twirp"
 	pb "github.com/zoido/twirp-grpc/api/v1"
 )
 
-// NewGreeterService creates new instance of the corresponding GRPC request handler.
+// NewGreeterService creates new instance of the corresponding Greeter request handler.
 func NewGreeterService() pb.GreeterServiceServer {
 	return &greeterService{}
 }
@@ -19,18 +18,11 @@ type greeterService struct{}
 
 func (*greeterService) GetGreeting(_ context.Context, req *pb.GetGreetingRequest) (*pb.GetGreetingResponse, error) {
 	if req.Name == "Dr.Evil" {
-		err := twirp.NewError(twirp.PermissionDenied, "Dr.Evil's not welcome here.")
-		log.Error().
-			Err(err).
-			Send()
-		return nil, err
+		return nil, twirp.NewError(twirp.PermissionDenied, "Dr.Evil's not welcome here.")
 	}
 	return &pb.GetGreetingResponse{
 		Greeting: &pb.Greeting{
 			Message: fmt.Sprintf("Hello %s!", req.Name),
 		},
 	}, nil
-}
-
-type err struct {
 }
